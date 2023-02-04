36b797bbb2c54dfadbf7a190727e648bc7e4d0df
PedroCandia <pedro.candiavz@pertendo.com>
2023-02-03 22:19:22 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pomoloft',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
